f573145e632974d8aa29d0673708e47ec505c99a
Roy Barton <roy.barton@cookmedical.com>
2019-12-13 15:36:30 +0000

    Create Constructor For AccountController

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -15,5 +15,11 @@ namespace WishList.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
     }
 }
